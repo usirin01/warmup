e7598c4157ca0ae82736c7e3419ac050469e8468
usirin01 <usirin2021@gmail.com>
2023-08-23 09:46:57 +0200

    some comment lines were added

--- week 3_Combining_Loops/app.js
+++ week 3_Combining_Loops/app.js
@@ -2,10 +2,14 @@ var grades= [2, 5, , , 9, 8, , 8];
 var sum = 0;
 var count = 0;//we want to divide really number
 
-if (grades.length > 0){
-  for (index = 0; index < grades.length; index++){
-    if (grades[index] != undefined){ //This line is important
-		  sum += grades[index]; 
+if (grades.length > 0)
+{
+  for (index = 0; index < grades.length; index++)
+  {
+    if (grades[index] != undefined)//This line is important
+
+    { 
+		      sum += grades[index]; 
           count = count + 1;//we want to divide really number
     }
   }
