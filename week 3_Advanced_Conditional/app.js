7fa745f089addb75455f157ca0a73547703fcf54
usirin01 <usirin2021@gmail.com>
2023-08-25 09:12:23 +0200

    about nested

---
+++ week 3_Advanced_Conditional/app.js
@@ -0,0 +1,9 @@
+var namen = prompt("What is your name?")
+
+if (namen.lenght != 0){
+    if (namen == "Umit")
+    document.write("what a beautiful name");
+    elsedocument.write("Hello" + namen);
+}
+else
+documen.write("Feeling shy?");
