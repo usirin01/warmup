f9a7fccbbbe7ad0b580f03f6994111b6a5e977ed
usirin01 <usirin2021@gmail.com>
2023-08-31 11:32:39 +0200

    updated

--- week 4/week 4_forms/app.js
+++ week 4/week 4_forms/app.js
@@ -1,14 +1,12 @@
-function nicknameFunction(){
+function nicknameFunction() {
     if (document.getElementById("yesNick").checked)
     {
-        document.getElementsById("nick").style.displa = "inline"
-
+        document.getElementById("nick").style.display = "inline";
         document.getElementById("nickname").setAttribute("required",true);
     }
     else
     {
         document.getElementById("nickname").removeAttribute("required");
         document.getElementById("nick").style.display = "none";
-
     }
 }
